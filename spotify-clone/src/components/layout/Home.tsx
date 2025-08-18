2e241947ba5ecf532a60506aa7341ecb806f1cc2
Facundo Chifflet <[200~>
2025-08-18 12:19:59 -0300

    Agregar música y control de volúmen

--- spotify-clone/src/components/layout/Home.tsx
+++ spotify-clone/src/components/layout/Home.tsx
@@ -6,6 +6,7 @@ import Navbar from "./Navbar"
 import { useState } from "react"
 import type { Song } from "../../types/songType"
 
+
 function Home() {
     const [currentSong, setCurrentSong] = useState<Song | null>(null);
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
@@ -13,7 +14,6 @@ function Home() {
     function togglePlay() {
         setIsPlaying((prev) => !prev);
     }
-
     return (
         <div className="flex flex-col justify-center items-center bg-black w-screen h-screen">
             <Navbar />
@@ -21,13 +21,18 @@ function Home() {
                 <div className="flex">
                     <SideBar />
                     <Display setCurrentSong={setCurrentSong} setIsPlaying={setIsPlaying} />
-                    <SongDetailSidebar titleSong="Survival" artist="Muse" titleAlbum="FFP Muse" src="https://i.scdn.co/image/ab67616d0000b273fc192c54d1823a04ffb6c8c9" />
+                    <SongDetailSidebar 
+                        titleSong="Survival" 
+                        artist="Muse" 
+                        titleAlbum="FFP Muse" 
+                        src="https://i.scdn.co/image/ab67616d0000b273fc192c54d1823a04ffb6c8c9" 
+                    />
                 </div>
             </div>
             <Player currentSong={currentSong} isPlaying={isPlaying} togglePlay={togglePlay} />
+            
         </div>
     )
 };
-
 
 export default Home;
--- spotify-clone/src/components/layout/Player.tsx
+++ spotify-clone/src/components/layout/Player.tsx
@@ -1,6 +1,7 @@
 import { assets } from "../../assets/assets";
 import PlayerSongCard from "../cards/PlayerSongCard";
 import type { Song } from "../../types/songType";
+import { useEffect, useRef, useState } from "react";
 
 type PlayerProps = {
   currentSong?: Song | null;
@@ -9,6 +10,23 @@ type PlayerProps = {
 }
 
 function Player({ currentSong, isPlaying, togglePlay }: PlayerProps) {
+  const [volume, setVolume] = useState(1);
+  const duration = 0
+  const currentTime = 0
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+    useEffect(() => {
+        if (!audioRef.current) return;
+        if (isPlaying) audioRef.current.play();
+        else audioRef.current.pause();
+    }, [isPlaying]);
+
+    useEffect(() => {
+        if (!audioRef.current || !currentSong) return;
+        audioRef.current.src = currentSong.src;
+        if (isPlaying) audioRef.current.play();
+    }, [currentSong]);
+
   return (
     <footer className='h-[80px] w-screen bg-black flex items-center text-white px-4 relative'>
       <div className='flex'>
@@ -26,17 +44,18 @@ function Player({ currentSong, isPlaying, togglePlay }: PlayerProps) {
             <button className="cursor-pointer ml-0.5" onClick={togglePlay}>
               <img src={isPlaying ? assets.pause_icon : assets.play_icon} className="w-4 h-4" alt="Play/Pause control" />
             </button>
+            <audio src={assets.audio} ref={audioRef} />
           </div>
           <img className='w-4 cursor-pointer' src={assets.next_icon} alt="Next song control" />
           <img className='w-4 cursor-pointer' src={assets.loop_icon} alt="Loop control" />
         </div>
         <div className='flex items-center gap-4 mt-1'>
-          <p className="text-[14px]">0:15</p>
-          <input min={0} max={100} value={0} onChange={(e) => e.target.value} type="range" className="w-[500px] h-1 bg-gray-400 rounded-lg appearance-none" />
-          <p className="text-[14px]">4:20</p>
+          <p className="text-[14px]">{formatTime(currentTime)}</p>
+          <input min={0} max={duration} value={currentTime} type="range" className="w-[500px] h-1 hover:bg-green-500 bg-white accent-white rounded-lg appearance-none" />
+          <p className="text-[14px]">{formatTime(duration)}</p>
         </div>
       </div>
-      <div className="flex gap-6 items-center cursor-pointer ml-auto mt-2">
+      <div className="flex gap-5 items-center cursor-pointer ml-auto mt-2">
         <img src={assets.plays_icon} alt="Play" className="w-[18px] h-[18px]" />
         <img src={assets.mic_icon} alt="Lyrics" className="w-[18px] h-[18px]" />
         <img src={assets.queue_icon} alt="Queue" className="w-[18px] h-[18px]" />
@@ -45,12 +64,33 @@ function Player({ currentSong, isPlaying, togglePlay }: PlayerProps) {
         <div>
           <img src={assets.volume_icon} alt="Volume control" className="w-4 h-4" />
         </div>
-        <div className='w-[100px] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
-          <hr className='h-1 border-none w-0 bg-green-800 rounded-full' />
+        <div className='flex h-1 '>
+          <input className="bg-white hover:bg-green-500  rounded cursor-pointer accent-white appearance-none"
+            type="range"
+            min={0}
+            max={1}
+            step={0.01}
+            value={volume}
+            onChange={(e) => {
+              const newVolume = parseFloat(e.target.value) 
+              setVolume(newVolume)
+              if(audioRef.current) {
+                audioRef.current.volume = newVolume
+              }
+            }}
+          />
         </div>
         <img src={assets.zoom_icon} alt="Zoom" className="w-4 h-4" />
       </div>
     </footer>
   )
+}
+function formatTime(time: number) {
+  if (isNaN(time)) return "0:00";
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60)
+    .toString()
+    .padStart(2, "0");
+  return `${minutes}:${seconds}`;
 }
 export default Player;
